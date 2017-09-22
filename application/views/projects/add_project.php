31ab04a93554f1999f87e5767bc48d8b805442f1
Damianves <99033554@mydavinci.nl>
2017-09-22 14:33:26 +0200

    uitlijning code
    Uitlijning van de add_project pagina

--- application/views/projects/add_project.php
+++ application/views/projects/add_project.php
@@ -7,28 +7,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	    	<div class="row">
 	    		<div class="col-md-12">
 				  	<?php echo form_open('projects/add_project'); ?>
-						    <label for="title">Projectnaam</label>
-						<div class="form-group">
-						    <input type="input" name="name" required/>
-					    </div>
-						    <label for="text">klant</label>
-					    <div class="form-group">
-						    <input type="input" name="client" required/>
-						</div>
-						    <label for="text">docent</label>
-						<div class="form-group">
-						    <input type="input" name="teacher" required/>
-						</div>
-						    <label for="text">leden</label>
-						<div class="form-group">
-						    <input type="input" name="members" required/>
-						</div>
-						    <label for="text">Beschrijving</label>
-						<div class="form-group">
-						    <textarea name="description" required></textarea>
-						</div>
+						<label for="title">Projectnaam</label>
+							<div class="form-group">
+								<input type="input" name="name" required/>
+							</div>
+						<label for="text">klant</label>
+							<div class="form-group">
+								<input type="input" name="client" required/>
+							</div>
+						<label for="text">docent</label>
+							<div class="form-group">
+								<input type="input" name="teacher" required/>
+							</div>
+						<label for="text">leden</label>
+							<div class="form-group">
+								<input type="input" name="members" required/>
+							</div>
+						<label for="text">Beschrijving</label>
+							<div class="form-group">
+								<textarea name="description" required></textarea>
+							</div>
 						<input type="hidden" name="posted" value="1">
-					    <input type="submit" name="submit" value="Project toevoegen" />
+						<input type="submit" name="submit" value="Project toevoegen" />
 
 					</form>
 
