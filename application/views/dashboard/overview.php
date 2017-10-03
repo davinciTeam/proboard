9929bb4dd360aadfad690faf574905b4b5877e13
Remco de zwart <remcodezwart1997@gmail.com>
2017-10-03 11:34:15 +0200

    mokup dashboard

--- application/views/dashboard/overview.php
+++ application/views/dashboard/overview.php
@@ -4,10 +4,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div class="content-wrapper">
   <section class="content-header">
-    <div class="row white">
-      <div class="col-md-4">.col-md-4</div>
-      <div class="col-md-4">.col-md-4</div>
-      <div class="col-md-4">.col-md-4</div>
+    <div class="row cards white">
+      <div class="col-md-12">projecten</div>
+    </div>
+    <div class="row cards">
+      <div class="col-md-12">
+        <table class="table table-striped table-center">
+          <thead>
+            <tr>
+              <th>Projectnaam</th>
+              <th>Klant</th>
+              <th>docent</th>
+              <th>leden</th>
+              <th>eerst volgende code revieuw</th>
+              <th>eerst volgende iteratie bespreking</th>
+              <th>opmerking</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="red">
+              <td>project beheer</td>
+              <td>slemmer</td>
+              <td>slemmer</td>
+              <td>student 1, student 2</td>
+              <td>woensdag 11 uur</td>
+              <td>10-10-2017 13 uur</td>
+              <td></td>
+            </tr>
+            <tr class="orange">
+              <td>componenten beheer</td>
+              <td>de jong</td>
+              <td>de jong</td>
+              <td>student 3, student 4</td>
+              <td>maandag 11 uur</td>
+              <td>vrijdag 13 uur</td>
+              <td>erg lange laadtijd</td>
+            </tr>
+            <tr class="green">
+              <td>speur tocht</td>
+              <td>ruijter</td>
+              <td>ruijter</td>
+              <td>student 5, student 6</td>
+              <td>25-10-2017 13 uur</td>
+              <td>donderdag 11 uur</td>
+              <td>security workshop onvoldoende</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
     </div>
   </section>
 </div>
