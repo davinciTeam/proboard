f9c3e20b62fc5114a282ed61a9d10494aa1dc62c
Damianves <99033554@mydavinci.nl>
2017-10-24 12:14:10 +0200

    Tooltips
    added tooltips for the addappointment page en the memberspage

--- application/views/members/addMembers.php
+++ application/views/members/addMembers.php
@@ -12,23 +12,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     	<div class="addmember">
 		    <div class="container">
 		    	<div class="row">
-		    		<div class="col-md-12">
+		    		<div class="col-md-10">
 					  	<?php echo form_open('members/addMemberAction'); ?>
 							<label for="ovnumber">Ov-nummer</label>
 								<div class="input-group">
 									<input type="number" class="form-control" id="ovnumber" max="99999999" name="ovnumber" required/>
+									<div class="input-group-addon">
+										<i class="fa fa-question" data-toggle="tooltip"  aria-hidden="true" title="Alleen de nummers 0-9 toegestaan"></i>
+									</div>
 								</div>
 							<label for="name">Voornaam</label>
 								<div class="input-group">
 									<input type="text" class="form-control"  maxlength="100" name="name" required/>
+									<div class="input-group-addon">
+										<i class="fa fa-question" data-toggle="tooltip"  aria-hidden="true" title="Alleen de letters a-z .!? en spaties zijn toegestaan(niet hoofdlettergevoelig)"></i>
+									</div>
 								</div>
 							<label for="insertion">Tussenvoegsel</label>
 								<div class="input-group">
 									<input type="input" class="form-control" maxlength="100" name="insertion">
+									<div class="input-group-addon">
+										<i class="fa fa-question" data-toggle="tooltip"  aria-hidden="true" title="Alleen de letters a-z .!? en spaties zijn toegestaan(niet hoofdlettergevoelig)"></i>
+									</div>
 								</div>
 							<label for="lastname">Achternaam</label>
 								<div class="input-group">
 									<input type="input" class="form-control" maxlength="100" name="lastname" required/>
+									<div class="input-group-addon">
+										<i class="fa fa-question" data-toggle="tooltip"  aria-hidden="true" title="Alleen de letters a-z .!? en spaties zijn toegestaan(niet hoofdlettergevoelig)"></i>
+									</div>
 								</div>
 							<button type="submit" class="btn btn-blue">Studenten Aanmaken</button>
 						</form>
