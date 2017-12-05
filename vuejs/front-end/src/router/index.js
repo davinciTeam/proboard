6fa189361feab4cabb06955fea265c559fa2143e
Damianves <99033554@mydavinci.nl>
2017-12-05 14:59:14 +0100

    start with user overview

--- vuejs/front-end/src/router/index.js
+++ vuejs/front-end/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Restfull from 'vue-resource'
 import HelloWorld from '@/components/HelloWorld'
+import UsersOverview from '@/components/UsersOverview'
 
 Vue.use(Router)
 Vue.use(Restfull)
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    	path: '/users',
+    	name: 'UsersOverview',
+    	component: UsersOverview
     }
   ]
 })
