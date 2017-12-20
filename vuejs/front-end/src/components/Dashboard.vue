f246d9cd91aeae77ea7c559efc599f5336e65b0f
Remco de zwart <remcodezwart1997@gmail.com>
2017-12-20 11:08:48 +0100

    Added sorting to the dashboard

--- vuejs/front-end/src/components/Dashboard.vue
+++ vuejs/front-end/src/components/Dashboard.vue
@@ -16,9 +16,11 @@
       <div class="row"> 
         <h4>Legenda</h4>
         <div class="col-md-12">
-            <i class="ion-stop text-danger" aria-hidden="true"></i> = Is al geweest.
-            <i class="ion-stop text-warning" aria-hidden="true"></i> = Is Vandaag.
-            <i class="ion-stop text-success" aria-hidden="true"></i> = Moet nog komen.
+          <ol class="ist-group">
+            <li class="list-group-item" aria-hidden="true"><i class="ion-stop text-danger"></i> = Is al geweest.</li> 
+            <li class="list-group-item" aria-hidden="true"><i class="ion-stop text-warning"></i> = Is Vandaag.</li> 
+            <li class="list-group-item" aria-hidden="true"><i class="ion-stop text-success"></i> = Moet nog komen.</li> 
+          </ol>
         </div>
       </div> 
       <div class="row">
@@ -35,10 +37,15 @@
           </thead>
           <tbody>        
             <tr v-for="project in projects['body']['projects']">
-              <td>{{ project['name'] }}<span :title="project['description']" class="size-14 ion-android-textsms"></span></td>
+              <td>{{ project['name'] }}
+                <i :title="project['description']" class="size-14 ion-android-textsms"></i>
+              </td>
               <td>{{ project['client'] }}</td>
               <td>{{ project['teacher'] }}</td>
-              <td><p v-for="member in project['members']"> {{ member['name'] }} {{ member['insertion'] }} {{ member['lastname'] }}</p></td>
+              <td>
+                <span v-for="member in project['members']">{{ member['name'] }} {{ member['insertion'] }} {{ member['lastname'] }}
+                </span>
+              </td>
               <td>{{ project['iteration_start'] }}</td>
               <td>{{ project['code_review_start'] }}</td>
             </tr>
@@ -54,9 +61,9 @@ export default {
   name: 'dashboard',
   data () {
     return {
-      projects: 'loading',
-      order: 'DESC',
-      sorting: 'name'
+      projects: '',
+      sorting: '',
+      order: ''
     }
   },
   created: function () {
@@ -66,26 +73,24 @@ export default {
       this.projects = response;
 
     }, response => {
-      this.projects = 'failed';
+
     })
   },
   methods: {
     sort: function(event) {
-      this.sorting = event.originalTarget.nextElementSibling.attributes[0].value
-      this.$http.get('http://proboard/dashboard?order='+this.order+'&sort='+this.sorting).then(response => {
-    
-        if (this.order === 'DESC') {
-          this.order = 'ASC'
-        } else if (event.originalTarget.nextElementSibling.attributes[0].value != this.sorting || this.order === 'ASC') {
-          this.order = 'DESC'
-        }
-
-        this.projects = response;
+      if (this.sorting !== event.originalTarget.attributes[0].value || this.sorted === 'DESC') {
+        this.sorted = 'ASC';
+        this.sorting = event.originalTarget.attributes[0].value
         
-
-      }, response => {
-        this.projects = 'failed';
-      })
+        this.projects['body']['projects'].sort(function(a, b){
+          var sorting = event.originalTarget.attributes[0].value;
+          if (a[sorting].toLowerCase() < b[sorting].toLowerCase()) return -1
+          if (a[sorting].toLowerCase() > b[sorting].toLowerCase()) return 1
+        })
+      } else {
+        this.projects['body']['projects'].reverse();
+        this.order = 'DESC';
+      }
     }
   }
 }
