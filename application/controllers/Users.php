33ec107aa20c69c09b27ac04f9e649c11939f0d3
Raymond Blok <99034225@mydavinci.nl>
2018-01-17 03:00:45 +0100

    conflict

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -2,6 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends CI_Controller {
 
+	public $id = 0;
+
 	public function __construct()
    	{
    		parent::__construct();
@@ -22,44 +24,21 @@ class Users extends CI_Controller {
 		echo_json($response);
 	}
 
-	public function editUser($id = null)
-	{
-		if (empty($id)) {
-			redirect('users');
-		}
-		$this->load->model("ConfigModel", "config_model");
-
-		$data['user_data'] = $this->config_model->getUser($id);
-
-		if (!$data['user_data']) {
-			show_404();
-		}
-
-		// render('config/user_edit', $data);
-	}
-
-	public function NewUser()
-	{
-		$this->load->model("ConfigModel", "config_model");
-
-		render('config/user_new');
-	}
-
 	public function NewUserAction()
 	{
-		//$this->load->library('form_validation');
-		// $this->form_validation->set_rules('email', 'email', 'required|valid_email|is_unique[users.email]',	
-		// array('is_unique' => 'E-mail adres is al in gebruik','valid_email' => 'Voer een geldig email adres in','required' => 'Dit veld is verplicht'));
-		// $this->form_validation->set_rules('username', 'Username', 'required',	
-		// array('required' => 'Voer een gebruikersnaam in'));
-		// $this->form_validation->set_rules('name', 'name', 'required',	
-		// array('required' => 'Voer een naam in'));
+		//$data = get_input_params();
 
-		// if ($this->form_validation->run()) {
+		//$this->load->library('form_validation');
+		//$this->form_validation->set_rules('email', 'email', 'required|valid_email|is_unique[users.email]');
+		//$this->form_validation->set_rules('username', 'Username', 'required');
+		//$this->form_validation->set_rules('name', 'name', 'required');
+		//$this->form_validation->set_data($data);
+
+		//if ($this->form_validation->run()) {
 			$saveData = array(
-				"name" =>  $this->input->post('name'),
-				"username" => $this->input->post('username'),
-				"email" => $this->input->post('email')
+				"name" =>  $data['name'],
+				"username" => $data['username'],
+				"email" => $data['email']
 			);
 			$this->config_model->insertUser($saveData);
 		//}
@@ -67,28 +46,28 @@ class Users extends CI_Controller {
 
 	public function edit($id)
 	{
-		//$this->load->library('form_validation');
-		//$this->form_validation->set_rules('email', 'email', 'required|valid_email|is_unique[users.email]',	
-		//array('is_unique' => 'E-mail adres is al in gebruik','valid_email' => 'Voer een geldig email adres in','required' => 'Dit veld is verplicht'));
-		//$this->form_validation->set_rules('username', 'Username', 'required',	
-		//array('required' => 'Voer een gebruikersnaam in'));
-		//$this->form_validation->set_rules('name', 'name', 'required',	
-		//array('required' => 'Voer een naam in'));
+		$this->id = $id;
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('email', 'email', 'required|valid_email|callback_email_check');
+		$this->form_validation->set_rules('name', 'name', 'required');
 		
-		//if ($this->form_validation->run()) {
-			$saveData = array(
-				"name" => $this->input->post('name'),
-				"username" => $this->input->post('username'),
-				"email" => $this->input->post('email')
-			);
-		get_input_params();
+		$data = get_input_params();
+		$this->form_validation->set_data($data);
 
-		//}
-		var_dump($saveData);
-		var_dump($_POST);
-		var_dump($_GET);
-		var_dump($_REQUEST);
-		//$newId = $this->config_model->updateUser($saveData, $id);
+		if ( $this->form_validation->run() ) {
+			$saveData = array(
+				"name" => $data['name'],
+				"email" => $data['email']
+			);			
+			$this->config_model->updateUser($saveData, $id);
+		}
+
+		return false;
+	}
+
+	public function email_check($email)
+	{
+		return $this->config_model->emailValidation($email, $this->id);
 	}
 
 	public function saveProfile()
