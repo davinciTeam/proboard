ac626935e3ad103e9d1afadfb0faa524bb4cb021
Remco de zwart <remcodezwart1997@gmail.com>
2018-01-23 14:08:01 +0100

    Finished project overview  page

--- vuejs/src/components/ProjectsOverview.vue
+++ vuejs/src/components/ProjectsOverview.vue
@@ -12,15 +12,14 @@
               <th>Klant</th>
               <th>Docent</th>
               <th>Leden</th>
-              <th>links</th>
+              <th>Links</th>
+              <th>Acties</th>
               <th>Actief</th>
             </tr>
           </thead>
           <tbody>        
             <tr v-for="project in projects['projects']">
-              <td>{{ project['name'] }}<b-btn variant="light" v-b-popover.hover="project['description']" title="Beschrijving"><icon name="comment" title=""></icon>
-
-              </b-btn>
+              <td>{{ project['name'] }}<b-btn variant="light" v-b-popover.hover="project['description']" title="Beschrijving"><icon name="comment"></icon> <span v-for="tags in project['tags']"><b-badge variant="info">{{ tags['name'] }}</b-badge></span></b-btn>
               </td>
               <td>{{ project['client'] }}</td>
               <td>{{ project['teacher'] }}</td>
@@ -31,11 +30,19 @@
                 <a target="_blank" :href="project['git_url']">Repository url</a><br>
                 <a target="_blank" :href="project['project_url']">Project url</a>
               </td>
+              <td><b-button variant="primary" @click="openModalEditProject(project['id'])"><icon name="pencil"></icon></b-button></td>
               <td><icon name="check" class="text-success" v-if="project['active'] == '1'"></icon><icon v-else name="times" class="text-danger" ></icon></td>
             </tr>
           </tbody>
         </table>
       </div>
+    </div>
+    <div>
+      <b-modal hide-footer=true ref="editProject" id="edit_user" title="Gebruiker bewerken">
+        <label>gebruikersnaam</label><b-form-input v-model="name" type="text" placeholder="Vul een gebruikersNaam in"></b-form-input>
+        <label>email</label><b-form-input v-model="email" type="text" placeholder="Vul een email in"></b-form-input>
+        <b-button class="mx-auto" variant="success" @click="closeModalEditUser">Opslaan</b-button>
+      </b-modal>
     </div>
   </div>
 </template>
@@ -57,6 +64,14 @@
       }).catch(err => {
         console.log(err);
       })
+    },
+    methods: {
+      openModalEditProject (id) {      
+        this.$refs.editProject.show()
+      },
+      closeModalEditUser () {
+        this.$refs.editProject.hide()
+      }
     }
   }
 </script>
